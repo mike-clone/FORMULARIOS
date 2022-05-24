55ead6dfe6d9a7dc0460e790ee4cdfc845a34e8e
Kenny5140x <105194745+Kenny5140x@users.noreply.github.com>
2022-05-23 23:18:08 -0500

    FrmCleiner llenado

--- FORMULARIOS/FrmCleiner.Designer.cs
+++ FORMULARIOS/FrmCleiner.Designer.cs
@@ -29,12 +29,292 @@ namespace FORMULARIOS
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmCleiner));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.txtTelefonoTra = new System.Windows.Forms.TextBox();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.txtDireccionTra = new System.Windows.Forms.TextBox();
+            this.txtCargoTra = new System.Windows.Forms.TextBox();
+            this.txtNombTra = new System.Windows.Forms.TextBox();
+            this.txtDni = new System.Windows.Forms.TextBox();
+            this.txtIdtra = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(39, 358);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(737, 150);
+            this.dataGridView1.TabIndex = 38;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.Violet;
+            this.btnEliminar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEliminar.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Location = new System.Drawing.Point(432, 117);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 37;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.BackColor = System.Drawing.Color.Violet;
+            this.btnModificar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnModificar.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModificar.Location = new System.Drawing.Point(422, 66);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(85, 33);
+            this.btnModificar.TabIndex = 36;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = false;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.BackColor = System.Drawing.Color.Violet;
+            this.btnRegistrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRegistrar.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistrar.Location = new System.Drawing.Point(422, 28);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(85, 24);
+            this.btnRegistrar.TabIndex = 35;
+            this.btnRegistrar.Text = "REGISTRAR";
+            this.btnRegistrar.UseVisualStyleBackColor = false;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // txtTelefonoTra
+            // 
+            this.txtTelefonoTra.Font = new System.Drawing.Font("Century Schoolbook", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTelefonoTra.Location = new System.Drawing.Point(194, 293);
+            this.txtTelefonoTra.Multiline = true;
+            this.txtTelefonoTra.Name = "txtTelefonoTra";
+            this.txtTelefonoTra.Size = new System.Drawing.Size(124, 24);
+            this.txtTelefonoTra.TabIndex = 34;
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Font = new System.Drawing.Font("Century Schoolbook", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCorreo.Location = new System.Drawing.Point(194, 249);
+            this.txtCorreo.Multiline = true;
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(183, 24);
+            this.txtCorreo.TabIndex = 33;
+            // 
+            // txtDireccionTra
+            // 
+            this.txtDireccionTra.Font = new System.Drawing.Font("Century Schoolbook", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDireccionTra.Location = new System.Drawing.Point(194, 203);
+            this.txtDireccionTra.Multiline = true;
+            this.txtDireccionTra.Name = "txtDireccionTra";
+            this.txtDireccionTra.Size = new System.Drawing.Size(158, 24);
+            this.txtDireccionTra.TabIndex = 32;
+            // 
+            // txtCargoTra
+            // 
+            this.txtCargoTra.Font = new System.Drawing.Font("Century Schoolbook", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCargoTra.Location = new System.Drawing.Point(194, 159);
+            this.txtCargoTra.Multiline = true;
+            this.txtCargoTra.Name = "txtCargoTra";
+            this.txtCargoTra.Size = new System.Drawing.Size(133, 24);
+            this.txtCargoTra.TabIndex = 31;
+            // 
+            // txtNombTra
+            // 
+            this.txtNombTra.Font = new System.Drawing.Font("Century Schoolbook", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombTra.Location = new System.Drawing.Point(194, 108);
+            this.txtNombTra.Multiline = true;
+            this.txtNombTra.Name = "txtNombTra";
+            this.txtNombTra.Size = new System.Drawing.Size(216, 24);
+            this.txtNombTra.TabIndex = 30;
+            // 
+            // txtDni
+            // 
+            this.txtDni.Font = new System.Drawing.Font("Century Schoolbook", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDni.Location = new System.Drawing.Point(194, 68);
+            this.txtDni.Multiline = true;
+            this.txtDni.Name = "txtDni";
+            this.txtDni.Size = new System.Drawing.Size(124, 24);
+            this.txtDni.TabIndex = 29;
+            // 
+            // txtIdtra
+            // 
+            this.txtIdtra.Font = new System.Drawing.Font("Century Schoolbook", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIdtra.Location = new System.Drawing.Point(194, 28);
+            this.txtIdtra.Multiline = true;
+            this.txtIdtra.Name = "txtIdtra";
+            this.txtIdtra.Size = new System.Drawing.Size(115, 24);
+            this.txtIdtra.TabIndex = 28;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(90, 302);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(79, 15);
+            this.label7.TabIndex = 27;
+            this.label7.Text = "TELEFONO";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(137, 75);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 15);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "DNI";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(104, 117);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 15);
+            this.label5.TabIndex = 25;
+            this.label5.Text = "NOMBRE";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(113, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 15);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "CARGO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(85, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 15);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "DIRECCION";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(104, 258);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 15);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "CORREO";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(55, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 15);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "ID TRABAJADOR";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::FORMULARIOS.Properties.Resources._4490381;
+            this.pictureBox1.Location = new System.Drawing.Point(513, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(263, 236);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 39;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.Violet;
+            this.btnSalir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSalir.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(677, 319);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 40;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // FrmCleiner
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmCleiner";
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.txtTelefonoTra);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.txtDireccionTra);
+            this.Controls.Add(this.txtCargoTra);
+            this.Controls.Add(this.txtNombTra);
+            this.Controls.Add(this.txtDni);
+            this.Controls.Add(this.txtIdtra);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmCleiner";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TRABAJADORES";
+            this.Load += new System.EventHandler(this.FrmCleiner_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.TextBox txtTelefonoTra;
+        private System.Windows.Forms.TextBox txtCorreo;
+        private System.Windows.Forms.TextBox txtDireccionTra;
+        private System.Windows.Forms.TextBox txtCargoTra;
+        private System.Windows.Forms.TextBox txtNombTra;
+        private System.Windows.Forms.TextBox txtDni;
+        private System.Windows.Forms.TextBox txtIdtra;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSalir;
     }
 }
--- FORMULARIOS/FrmCleiner.cs
+++ FORMULARIOS/FrmCleiner.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace FORMULARIOS
 {
@@ -15,6 +16,85 @@ namespace FORMULARIOS
         public FrmCleiner()
         {
             InitializeComponent();
+            mostrar();
+        }
+        string conexion = "Data Source=DESKTOP-R0RTQRL;Initial Catalog=Cleiner;Integrated Security=True";
+        void Limpiar()
+        {
+            txtCargoTra.Clear();
+            txtCorreo.Clear();
+            txtDireccionTra.Clear();
+            txtDni.Clear();
+            txtIdtra.Clear();
+            txtNombTra.Clear();
+            txtTelefonoTra.Clear();
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection(conexion))
+            {
+                SqlCommand cmd = new SqlCommand("insert into Trabajador(IdTrabajador,dni ,Nom_Trabajador ,Cargo ,Direccion ,Correo ,Telefono) values(" + txtIdtra.Text + ",'" + txtDni.Text + "','" + txtNombTra.Text + "','" + txtCargoTra.Text + "','" + txtDireccionTra.Text + "','" + txtCorreo.Text + "','" + txtTelefonoTra.Text + "')", cn);
+                cmd.CommandType = CommandType.Text;
+                cn.Open();
+                cmd.ExecuteNonQuery();
+
+            }
+            Limpiar();
+            mostrar();
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection(conexion))
+            {
+
+                SqlCommand cmd = new SqlCommand("update Trabajador set dni='" + txtDni.Text + "' ,Nom_Trabajador='" + txtNombTra.Text + "' ,Cargo='" + txtCargoTra.Text + "' ,Direccion='" + txtDireccionTra.Text + "' ,Correo='" + txtCorreo.Text + "' ,Telefono='" + txtTelefonoTra.Text + "'  where IdTrabajador=" + txtIdtra.Text + " ", cn);
+                cmd.CommandType = CommandType.Text;
+                cn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            Limpiar();
+            mostrar();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection(conexion))
+            {
+
+                SqlCommand cmd = new SqlCommand("delete from Trabajador where IdTrabajador= " + txtIdtra.Text + "", cn);
+                cmd.CommandType = CommandType.Text;
+                cn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            Limpiar();
+            mostrar();
+        }
+
+        void mostrar()
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection cn = new SqlConnection(conexion))
+            {
+
+                SqlDataAdapter da = new SqlDataAdapter("select *from Trabajador", cn);
+                da.SelectCommand.CommandType = CommandType.Text;
+                cn.Open();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+
+        private void FrmCleiner_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
